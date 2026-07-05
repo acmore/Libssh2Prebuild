4598754ff24a049bb8bd2f7e0b1ffe452116202c
acmore <hezj07@gmail.com>
2026-07-05 19:30:52 +0800

    Build 1.11.1-openssl-3.5.7

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "CSSH",
-                      url: "https://github.com/DimaRU/Libssh2Prebuild/releases/download/1.11.0-OpenSSL-1-1-1w/CSSH-1.11.0-OpenSSL-1-1-1w.xcframework.zip",
-                      checksum: "f2a14236019b617019f522001c89ad5db2125e8f350cbcde5ed6e96f50842157")
+                      url: "https://github.com/acmore/Libssh2Prebuild/releases/download/1.11.1-openssl-3.5.7/CSSH-1.11.1-openssl-3.5.7.xcframework.zip",
+                      checksum: "8cda0a4a7e24f9de33ba98f26f4e16206658df8e7a5c2e0b7c874e2187b555fc")
     ]
 )
